87286d22db9572f3829187b0796ce1f608565087
Jasonparra21 <jeissonparra1212@gmail.com>
2022-10-31 00:04:02 -0500

    9

--- JEIKEPON.js
+++ JEIKEPON.js
@@ -1,12 +1,25 @@
-function petchoose(){
-    alert('you choose your pet')
+function startgame(){
+    let petButton = document.getElementById('pet-button')
+    petButton.addEventListener('click',petchoose)
 }
+let Pets=['Drawid','Orchiwet','Toprock','Vessptox','Frogblex','WhitePhoenix','Massivehummingbird','xcorpion'  ]
+function petchoose(){
+    let a = 0
+    for (var i = 0; i < 8 ; i++){
+        let pet = document.getElementById(Pets[i])
+        let spanPlayerPet =document.getElementById('playerPet')
+        if (pet.checked){
+            //alert('you choose ' + Pets[i])
+            spanPlayerPet.innerHTML = Pets[i]
+        } else {
+            a=a+1}
+    }
+    if (a==Pets.length) {
+        alert('Choose at least 1 pet')  
+    }
+}
+window.addEventListener('load',startgame)
 
-
-let petButton = document.getElementById('pet-button')
-
-
-petButton.addEventListener('click, petchoose')
 
 
 
