65160fa88c1a3d001db6d4a348ef2c90dd1ee072
Jasonparra21 <jeissonparra1212@gmail.com>
2022-12-29 00:56:40 -0500

    correccion errores :)

--- JEIKEPON.js
+++ JEIKEPON.js
@@ -1,23 +1,34 @@
-let Pets = ['Drawid','Orchiwet','Toprock','Vessptox','Frogblex','WhitePhoenix','Massivehummingbird','xcorpion']
-let Attacks =['Wind','Fire','Water','Earth' ]
+const restartButton = document.getElementById('Restart')
+const chooseAttack = document.getElementById('Choose_attack')
+const petButton = document.getElementById('pet-button')
+const windButton = document.getElementById('wind-button')
+const fireButton = document.getElementById('fire-button')
+const waterButton = document.getElementById('water-button')
+const earthButton = document.getElementById('earth-button')
+const Pets = ['Drawid','Orchiwet','Toprock','Vessptox','Frogblex','WhitePhoenix','RazorEagle','Xcorpion']
+const Attacks =['Wind','Fire','Water','Earth' ]
+const choosePet = document.getElementById('Choose_pet')
+const spanPlayerPet =document.getElementById('playerPet')
+const spanEnemysPet = document.getElementById('enemysPet')
+
+const spanPlayerLives = document.getElementById('playerLives')
+const spanEnemysLives = document.getElementById('enemyLives')
+
+const messageSection = document.getElementById('Result')
+const player_attacks = document.getElementById('player-attacks')
+const enemy_attacks = document.getElementById('enemy-attacks')
+
 let playerattack 
 let enemyAttack
 let playerLives = 3 
 let enemyLives  = 3
 function startgame(){
-    let restartButton = document.getElementById('Restart')
     restartButton.style.display = 'none'
-    let chooseAttack = document.getElementById('Choose_attack')
     chooseAttack.style.display  = 'none'
-    let petButton = document.getElementById('pet-button')
     petButton.addEventListener('click', petchoose)
-    let windButton = document.getElementById('wind-button')
     windButton.addEventListener('click', windAttack)
-    let fireButton = document.getElementById('fire-button')
     fireButton.addEventListener('click', fireAttack)
-    let waterButton = document.getElementById('water-button')
     waterButton.addEventListener('click', waterAttack)
-    let earthButton = document.getElementById('earth-button')
     earthButton.addEventListener('click', earthAttack)
     restartButton.addEventListener('click', restart)
 }
@@ -25,22 +36,19 @@ function randomselector (min,max){
     return Math.floor(Math.random()*(max-min+1)+min)
 }
 function petchoose(){
-    let choosePet = document.getElementById('Choose_pet')
     choosePet.style.display  = 'none'
-    let chooseAttack = document.getElementById('Choose_attack')
     chooseAttack.style.display  = 'flex'
+
     let a = 0
     for (var i = 0; i < 8 ; i++){
         let pet = document.getElementById(Pets[i])
-        let spanPlayerPet =document.getElementById('playerPet')
         if (pet.checked){
-            //alert('you choose ' + Pets[i])
             spanPlayerPet.innerHTML = Pets[i]
         } else {
             a=a+1
-            if (a==6) {
-                alert('Choose at least 1 pet')
-                location.reload()
+            if (a==8) {
+               alert('Choose at least 1 pet')
+               location.reload()
             }
         }
     }
@@ -48,10 +56,7 @@ function petchoose(){
 }
  function enemyspetchoose(){
     let enemy = randomselector(1,8)
-    let Enemyspet = document.getElementById(Pets[enemy-1])
-    let spanEnemysPet = document.getElementById('enemysPet')
-    spanEnemysPet.innerHTML = Enemyspet
-    
+    spanEnemysPet.innerHTML = Pets[enemy]
 }
 function windAttack(){
     playerattack = 'Wind'
@@ -75,8 +80,6 @@ function enemyRandomAttack (){
     combat()
 }
 function combat() {
-    let spanPlayerLives = document.getElementById('playerLives')
-    let spanEnemysLives = document.getElementById('enemyLives')
     if (playerattack == enemyAttack){
         messageCreator('Draw!')
     }else if ((playerattack == Attacks[2] && enemyAttack == Attacks[1]) || (playerattack == Attacks[2] && enemyAttack == Attacks[0])||(playerattack == Attacks[1] && enemyAttack == Attacks[3])||(playerattack == Attacks[1] && enemyAttack == Attacks[0])|| (playerattack == Attacks[0] && enemyAttack == Attacks[3])||(playerattack == Attacks[3] && enemyAttack == Attacks[2])){
@@ -98,13 +101,8 @@ function countLives(){
     }
 }
 function messageCreator(result){
-    let messageSection = document.getElementById('Result')
-    let player_attacks = document.getElementById('player-attacks')
-    let enemy_attacks = document.getElementById('enemy-attacks')
-    
     let newPlayerAttack= document.createElement('p')
     let newEnemyAttack = document.createElement('p')
-    
     messageSection.innerHTML = result
     newPlayerAttack.innerHTML = playerattack
     newEnemyAttack.innerHTML = enemyAttack
@@ -113,17 +111,11 @@ function messageCreator(result){
     enemy_attacks.appendChild(newEnemyAttack)
 }
 function finalMessage(finalResult){
-    let messageSection = document.getElementById('Result')
     messageSection.innerHTML = finalResult
-    let windButton = document.getElementById('wind-button')
     windButton.disabled =true
-    let fireButton = document.getElementById('fire-button')
     fireButton.disabled =true
-    let waterButton = document.getElementById('water-button')
     waterButton.disabled =true
-    let earthButton = document.getElementById('earth-button')
     earthButton.disabled =true
-    let restartButton = document.getElementById('Restart')
     restartButton.style.display ='block'
 }
 function restart(){
